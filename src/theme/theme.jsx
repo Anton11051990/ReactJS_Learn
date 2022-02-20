7a7e0b4fdd406a3598f3d2954f2a919cb0c68671
Anton <Rogachev.a.e.90@mail.ru>
2022-02-20 13:26:20 +0300

    1.4

---
+++ src/theme/theme.jsx
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material/styles";
+
+import { blue } from "@mui/material/colors";
+
+const theme = createTheme({
+  palette: {
+    primary: blue,
+  },
+});
+
+export default theme;
