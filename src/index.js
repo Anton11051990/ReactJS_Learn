9064b78df29e4dca364455793a3efaa547336b3a
Anton <Rogachev.a.e.90@mail.ru>
2022-03-06 17:35:45 +0300

    полностью переделанный проект

--- src/index.js
+++ src/index.js
@@ -1,19 +1,44 @@
-import React from "react";
 import ReactDOM from "react-dom";
-import reportWebVitals from "./reportWebVitals";
-import "./index.css";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material";
+import React from "react";
+import { ChatPage } from "./pages";
+import { HomePage } from "./pages";
+import "./global.css";
+import App from "./App";
+import { store } from "./store/create-store";
+import { Provider } from "react-redux";
 
-import App from "./Components/App";
-import { BrowserRouter } from "react-router-dom";
+const light = createTheme({
+  theme: {
+    color: "red",
+  },
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <ThemeProvider theme={light}>
+      <Switch>
+        <Route path="/chat">
+          <ChatPage />
+        </Route>
 
+        <Route path="/profile">
+          <Provider store={store}>
+            <App />
+          </Provider>
+        </Route>
+
+        <Route path="/">
+          <HomePage />
+        </Route>
+
+        <Route path="*">
+          <h1>404 page</h1>
+          <Link to="/chat">go to Chat</Link>
+        </Route>
+      </Switch>
+    </ThemeProvider>
+  </BrowserRouter>,
   document.getElementById("root")
 );
-
-reportWebVitals();
